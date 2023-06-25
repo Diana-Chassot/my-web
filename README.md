6407b42e92ad7a3e6a4a585520dacc517793cd80
Diana-Chassot <diana.chassot25@gmail.com>
2023-06-25 12:33:36 +0200

    my formated web

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# my-web
-my web page
+# My web
+* Diana Chassot
+### Landing page with contact form using "Formspree" .Page provide information about me, my skills and projects. 
+
+
+#### Technologies used in the project:
+* HTML;
+* CSS;
+* JavaScript;
+* Formspree
+
+## Installation
+
+There is no installation required for this website. Simply open the `index.html` file in a web browser to view it.
+
+## Usage
+
+You can navigate through the different sections of the landing page by scrolling or using the navigation menu.
--- js/script.js
+++ js/script.js
@@ -1,48 +1,65 @@
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function() {
-      document.querySelector("body").classList.add("loaded");
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.querySelector("body").classList.add("loaded");
   }, 1000)
 });
 
-const btnNav = document.querySelector(".header__icon-mobile");
-const navList = document.querySelector(".header__list-mobile");
-btnNav.addEventListener("click", togleClassActive);
-
-function togleClassActive() {
-  btnNav.classList.toggle("open");
-  if (btnNav.classList.contains("open")) {
-    navList.style.display = "flex";
-    btnNav.classList.remove("fa-spin");
+function toogleNav() {
+  const navList = document.querySelector(".header__list");
+  const navIcon = document.querySelector(".header__icon");
+  navList.classList.toggle("show");
+  if (navList.classList.contains("show")){
+    navIcon.classList.remove('fa-spin');
+  }else{
+    navIcon.classList.add('fa-spin');
   }
-  else {
-    navList.style.display = "none";
-    btnNav.classList.add("fa-spin");
-  }
-}
-window.addEventListener('resize', function () {
-  if (btnNav.classList.contains('open')) {
-    navList.style.display = 'none';
-    navList.classList.remove('open');
-  }
-});
-document.addEventListener("click", function (e) {
-  if (!navList.contains(e.target) && !btnNav.contains(e.target)) {
-    btnNav.classList.remove("open");
-    btnNav.classList.add("fa-spin");
-    navList.style.display = "none";
-  }
-});
-
-
-const paralaxElem = document.querySelectorAll('.paralax-elem');
-window.addEventListener('scroll', () => addParallax(paralaxElem));
-
-
-function addParallax(elem){
-  let scroll = window.pageYOffset;
-  elem.forEach(el => {
-    let offset = el.offsetTop;
-    let distance = (offset - scroll) * 0.1;
-    el.style.transform = 'translateY(' + distance + 'px)';
+  document.addEventListener('click', function(event) {
+    const isClickInsideMenu = navList.contains(event.target);
+    const isClickInsideNavIcon = navIcon.contains(event.target);
+    if (!isClickInsideMenu && !isClickInsideNavIcon) {
+      navList.classList.remove("show");
+      navIcon.classList.add('fa-spin');
+    }
   });
 }
+
+(function () {
+  const aboutTabsContainer = document.querySelector(".about__right");
+  const projectTabsContainer = document.querySelector(".projects");
+
+  const navList = document.querySelector(".header__list");
+  const navIcon = document.querySelector(".header__icon");
+  navList.classList.contains("show") ?  navIcon.classList.remove("fa-spin") : navIcon.classList.add("fa-spin");
+  navIcon.addEventListener("click", toogleNav);
+  aboutTabsContainer.addEventListener("click", (e) => {
+    handleTabs(e, ".tab-content")
+  });
+  projectTabsContainer.addEventListener("click", (e) => {
+    handleTabs(e, ".project__card-content")
+  });
+})();
+
+
+function handleTabs(e, contentSelector) {
+  const tabButton = document.querySelectorAll(".tab-btn");
+  const contents = document.querySelectorAll(contentSelector);
+  const id = e.target.dataset.id;
+
+  if (id) {
+    tabButton.forEach(btn => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    contents.forEach(content => {
+      content.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+}
+
+
+
+
+
